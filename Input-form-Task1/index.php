77a77b0525e5168be942a149c13517ba2eaab234
ramneek-ct <156078197+ramneek-ct@users.noreply.github.com>
2024-02-01 13:28:55 +0530

    Update index.php

--- Input-form-Task1/index.php
+++ Input-form-Task1/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title> Input Form </title>
-        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
         
         <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-        <script src="assets/index.js"></script>
+        <script src="assets/js/index.js"></script>
     </head>
     <body>
         <div class="main-container">
