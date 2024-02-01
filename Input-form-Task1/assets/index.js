494d232174e4b62e8f6c6edd107f5bc85e8b79c2
ramneek-ct <156078197+ramneek-ct@users.noreply.github.com>
2024-02-01 13:29:52 +0530

    Update index.js

--- Input-form-Task1/assets/index.js
+++ Input-form-Task1/assets/index.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-$(".submit-button").click(function() {
-   var firstname = $("#firstname").val();
-   var lastname = $("#lastname").val();
-   var phone = $("#phone").val();
-   alert("Your response has been recorded!");
-   
-   $.ajax({
-    type: 'POST',
-    url: 'info.php',
-    data: {
-        firstname: firstname,
-        lastname: lastname,
-        phone: phone
-    },
-    success: function(response){
-        var $result = response.firstname + " " + response.lastname + " " + response.phone;
-        alert($result);
-        }
+    $(".submit-button").click(function() {
+       var f_name = $("#firstname").val();
+       var l_name = $("#lastname").val();
+       var ph_no = $("#phone").val();
+       alert("Your response has been recorded!");
+       
+       $.ajax({
+        type: 'POST',
+        url: 'info.php',
+        data: {
+            first_n: f_name,
+            last_n: l_name,
+            phn: ph_no
+        },
+        success: function(response){
+            var $result = response.firstname + " " + response.lastname + " " + response.phone;
+            alert($result);
+            }
+        });
     });
-});
-});
+    });
+    
